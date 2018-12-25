4fea9f807e22d7ca3a7f997256f6f176b329e8bc
Rusi Parikh <sunnyparikh106@gmail.com>
2018-12-25 23:35:50 +0530

    - data type changed in raw_material table.

--- sql/update.sql
+++ sql/update.sql
@@ -7,3 +7,9 @@ ALTER TABLE `cat1801001`.`food_type` CHANGE `id` `id` VARCHAR(11) NOT NULL;
 ALTER TABLE `raw_material_main` CHANGE `id` `id` VARCHAR(7) NOT NULL;
 ALTER TABLE `raw_material_sub` CHANGE `id` `id` VARCHAR(7) NOT NULL;
 ALTER TABLE `raw_material_sub` CHANGE `fk_raw_material_main_id` `fk_raw_material_main_id` VARCHAR(7) NOT NULL;
+
+-- Added by Rusi
+-- changed id from int to varchar as per structure in raw material master.
+ALTER TABLE `raw_material` CHANGE `id` `id` VARCHAR(7) NOT NULL, 
+CHANGE `fk_raw_material_main_id` `fk_raw_material_main_id` VARCHAR(7) NULL, 
+CHANGE `fk_raw_material_sub_id` `fk_raw_material_sub_id` VARCHAR(7) NULL;
